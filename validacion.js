6fddd8f36b9beec7cae0ad02a3d9b09fd8622c76
mibichoandafugao <augustohenao1234@gmail.com>
2024-05-28 01:14:42 -0500

    Challenge finalizado

--- validacion.js
+++ validacion.js
@@ -1,1 +1,20 @@
 //Haz tú validación en javascript acá
+
+const nombreUsuario = document.getElementById('nombre')
+const emailUsuario = document.getElementById('email')
+const asuntoUsuario = document.getElementById('asunto')
+const mensajeUsuario = document.getElementById('mensage')
+
+const validarUsuario = () => {
+  if (
+    nombreUsuario.value === '' ||
+    emailUsuario.value === '' ||
+    asuntoUsuario.value === '' ||
+    mensajeUsuario.value === ''
+  ) {
+    alert('Por favor, rellene todos los campos')
+    return
+  } else {
+    alert('Datos enviados correctamente')
+  }
+}
